852ee7f4d73ee1d300d8c3be0044bd01bd4be65d
flashdin <flashdin007@gmail.com>
2023-01-03 16:14:49 +0700

    update code & file

--- src/main/java/com/example/pdfgenerator/utils/PdfCreator.java
+++ src/main/java/com/example/pdfgenerator/utils/PdfCreator.java
@@ -14,11 +14,21 @@ import java.util.regex.Pattern;
 
 public class PdfCreator {
 
+    private final String FIRST_LAST_SQUAREBRACKET_PATTERN = "\\[(.*)\\]";
+    private final String SQUAREBRACKET_PATTERN = "\\[([^\\[.]+?)\\]";
+    private final String FIRST_LAST_ROUNDBRACKET_PATTERN = "\\((.*)\\)";
+
     @SneakyThrows
     public void createPdf(String source, String dest, Map<String, Object> data) {
         File file = ResourceUtils.getFile("classpath:templates/" + source);
         String str = Files.readString(file.toPath());
-        Pattern pattern = Pattern.compile("\\[(.*)\\]");
+        this.createPdfFromString(str, dest, data);
+    }
+
+    @SneakyThrows
+    public void createPdfFromString(String html, String dest, Map<String, Object> data) {
+        String str = html;
+        Pattern pattern = Pattern.compile(this.FIRST_LAST_SQUAREBRACKET_PATTERN);
         // under [] "\\[([^\\[.]+?)\\]"
         // under html tag tr "(?s)<tr\\b.*?>(.*?)<\\/tr>"
         Matcher matcher = pattern.matcher(str);
@@ -27,7 +37,7 @@ public class PdfCreator {
         while (matcher.find()) {
             String target = matcher.group(1);
             if (target.indexOf("@foreach") != -1) {
-                Pattern pattern1 = Pattern.compile("\\((.*)\\)");
+                Pattern pattern1 = Pattern.compile(FIRST_LAST_ROUNDBRACKET_PATTERN);
                 Matcher matcher1 = pattern1.matcher(target);
                 while (matcher1.find()) {
                     String[] target1 = matcher1.group(1).split(",");
@@ -35,14 +45,13 @@ public class PdfCreator {
                     if (!ObjectUtils.isEmpty(targetValue1) && !targetValue1.isEmpty()) {
                         for (Map<String, Object> t : targetValue1) {
                             String res = target1[1];
-                            Pattern pattern2 = Pattern.compile("\\[([^\\[.]+?)\\]");
+                            Pattern pattern2 = Pattern.compile(SQUAREBRACKET_PATTERN);
                             Matcher matcher2 = pattern2.matcher(res);
                             while (matcher2.find()) {
                                 res = res.replaceAll("\\[(" + matcher2.group(1) + ")\\]", (String) t.get(matcher2.group(1)));
                             }
                             sb1.append(res);
                         }
-                        System.out.println(sb1.toString());
                         matcher.appendReplacement(sb, sb1.toString());
                     }
                 }
@@ -51,23 +60,6 @@ public class PdfCreator {
                 if (!ObjectUtils.isEmpty(targetValue)) {
                     matcher.appendReplacement(sb, (String) targetValue);
                 }
-            }
-        }
-        matcher.appendTail(sb);
-        HtmlConverter.convertToPdf(sb.toString(), new FileOutputStream(dest));
-    }
-
-    @SneakyThrows
-    public void createPdfFromString(String html, String dest, Map<String, Object> data) {
-        String str = html;
-        Pattern pattern = Pattern.compile("\\[(.*)\\]");
-        Matcher matcher = pattern.matcher(str);
-        StringBuilder sb = new StringBuilder();
-        while (matcher.find()) {
-            String target = matcher.group(1);
-            Object targetValue = data.get(target);
-            if (!ObjectUtils.isEmpty(targetValue)) {
-                matcher.appendReplacement(sb, (String) targetValue);
             }
         }
         matcher.appendTail(sb);
